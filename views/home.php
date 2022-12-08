ee9a6c856713b4e16b7eac63ef9254efd53285d8
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-12-08 16:22:32 +0200

    Update: page hero and menu
    Created and styled the page's hero and styled it. Also created a menu and mad it responsive

---
+++ views/home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <script src="https://kit.fontawesome.com/648e6e8434.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="../includes/css/home.css">
+</head>
+
+<body>
+    <div class="hero">
+
+        <img src="../includes/images/2359910009_9_1_1.jpg" alt="hero">
+
+        <div id="hero-heading">
+            <p id="hero-title">CHASE DREAMS ... IN HEELS OF COURSE</p>
+            <p>Since 2022, SHO-E has been making women look and feel confident.</p>
+            <input type="button" href="#" id="hero-button-shop" name="hero-button-shop" value="SHOP">
+        </div>
+    </div>
+
+    <div class="header">
+        <div class="logo">
+            HEELS
+
+        </div>
+        <div class="menu">
+
+            <ul>
+                <li>HOME</li>
+                <li>LOOKBOOK</li>
+                <li>SHOP</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+
+            </ul>
+
+        </div>
+        <div class="cart-options">
+          <ul>
+            <li>SEARCH</li>
+            <li>CART</li>
+            <li>PROFILE</li>
+          </ul>
+
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
