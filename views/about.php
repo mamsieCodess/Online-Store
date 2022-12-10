42bea4753ee26ad096890e0dd97eddc8a7900ea7
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-12-10 21:24:31 +0200

    Update about.php

--- views/about.php
+++ views/about.php
@@ -19,29 +19,29 @@
         <h2>Mission and Vision Statement</h2>
         <p>
         <h5> Identifying Market Preferences</h5>
-        Zara declares in the first part of its mission statement that it intends to give customers what they want, and Zara definitely means it. Other high fashion brands do not really make an effort to identify the preferences of the target market. This has actually been the culture in high fashion for decades. The designers simply come up with apparel that matches their own tastes and styles and expect that people will buy it simply because it is high fashion.
+        Slay Dragons declares in the first part of its mission statement that it intends to give customers what they want, and Slay Dragons definitely means it. Other high fashion brands do not really make an effort to identify the preferences of the target market. This has actually been the culture in high fashion for decades. The designers simply come up with apparel that matches their own tastes and styles and expect that people will buy it simply because it is high fashion.
 
-        The approach of Zara is exactly the opposite. While Zara does have a strong team of designers with impeccable talent, it also listens carefully to what the customers want. Zara has a very interactive social media presence, and it utilizes this to reach out to its client base, communicating with it to get a feel of what the market is going to buy. Even in physical stores, Zara staff take note of what customers like and do not like. The information is relayed to the design team, who makes sure that the client's requests and suggestions are incorporated into the brand's next collections.
+        The approach of Slay Dragons is exactly the opposite. While Slay Dragons does have a strong team of designers with impeccable talent, it also listens carefully to what the customers want. Slay Dragons has a very interactive social media presence, and it utilizes this to reach out to its client base, communicating with it to get a feel of what the market is going to buy. Even in physical stores, Slay Dragons staff take note of what customers like and do not like. The information is relayed to the design team, who makes sure that the client's requests and suggestions are incorporated into the brand's next collections.
 
         <h5>Fast Fashion Strategy</h5>
-        In the second part of the Zara mission statement, the company aims for the speedy delivery of the market's demand. In other words, it does not waste any time in its production process. On average, it takes only two weeks for the Zara team to complete a collection that includes 500 or so designs. This includes everything from design conceptualization to delivering the clothing to the stores. This is what is now known in the retail industry as the fast fashion strategy.
+        In the second part of the Slay Dragons mission statement, the company aims for the speedy delivery of the market's demand. In other words, it does not waste any time in its production process. On average, it takes only two weeks for the Slay Dragons team to complete a collection that includes 500 or so designs. This includes everything from design conceptualization to delivering the clothing to the stores. This is what is now known in the retail industry as the fast fashion strategy.
 
-        The Zara vision statement is "to contribute to the sustainable development of society and that of the environment with which we interact."
+        The Slay Dragons vision statement is "to contribute to the sustainable development of society and that of the environment with which we interact."
 
-        A company's vision statement reflects its long-term goals and how it sees itself in the distant future. Like its mission statement, the Zara vision statement is clear and concise, and it also reveals a side of the fashion brand that we don't easily see while we are buying its clothes. The statement shows how the company puts a priority on society and the environment more than it does on sales and profits.
+        A company's vision statement reflects its long-term goals and how it sees itself in the distant future. Like its mission statement, the Slay Dragons vision statement is clear and concise, and it also reveals a side of the fashion brand that we don't easily see while we are buying its clothes. The statement shows how the company puts a priority on society and the environment more than it does on sales and profits.
 
         <h5>Societal Development</h5>
-        Zara takes its corporate social responsibility very seriously. It is constantly involved in campaigns for the improvement of people's lives and actively supports programs that give back to the community. In the light of recent criticisms received by the fashion industry regarding the use of inappropriate labor practices in its third-world country facilities, brands like Zara are taking the extra effort to declare their support for societal development and sustainability.
+        Slay Dragons takes its corporate social responsibility very seriously. It is constantly involved in campaigns for the improvement of people's lives and actively supports programs that give back to the community. In the light of recent criticisms received by the fashion industry regarding the use of inappropriate labor practices in its third-world country facilities, brands like Slay Dragons are taking the extra effort to declare their support for societal development and sustainability.
 
         <h5>Environmental Sustainability</h5>
-        The environment is also very important to the Zara brand. It is quite known for its active stance in environmental campaigns. It has long denounced the use of plastic and does not include unnecessary labels on its clothing. Its manufacturing process is also completely toxic-free.</p>
+        The environment is also very important to the Slay Dragons brand. It is quite known for its active stance in environmental campaigns. It has long denounced the use of plastic and does not include unnecessary labels on its clothing. Its manufacturing process is also completely toxic-free.</p>
 
     </section>
 
     <section id="information">
         <h2>Business brief</h2>
        
-        <p>Zara is one of the largest international fashion companies. It belongs to Inditex, one of the world’s largest distribution groups.
+        <p>Slay Dragons is one of the largest international fashion companies. It belongs to Inditex, one of the world’s largest distribution groups.
 
             The customer is at the heart of our unique business model, which includes design, production, distribution and sales through our extensive retail network.
 
