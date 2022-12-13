56b970e0d131bfcde253575d151d2acb369772ea
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-12-13 20:39:36 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Lexend+Giga:wght@400;600&family=Montserrat+Alternates&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="includes/css/styles.css">
+    <script src="https://kit.fontawesome.com/648e6e8434.js" crossorigin="anonymous"></script>
+    
+    <style>
+        .about-page {
+            margin: 110px 0;
+            height: max-content;
+            color: white;
+            padding: 20px;
+
+        }
+
+        .about-page h2 {
+            text-align: center;
+            color: #ff1e00;
+            font-family: "Lexend Giga", sans-serif;
+
+        }
+
+        #team>div {
+            padding: 10px;
+        }
+
+        #team span {
+            color: #ff1e00;
+        }
+
+        #team {
+            display: flex;
+            justify-content: center;
+            font-family: 'Julius Sans One', sans-serif;
+        }
+
+        #team img {
+            width: 200px;
+            height: 250px;
+        }
+
+        #mission_vision {
+            text-align: center;
+
+
+        }
+
+        .about-page h5 {
+            padding: 20px;
+        }
+
+        .about-page section {
+            padding: 20px;
+        }
+
+        @media screen and (max-width: 900px) {
+
+            .about-page {
+        margin-top: 170px;
+    }
+        }
+    </style>
+</head>
+
+<body>
+<?php include_once __DIR__ . "/header.php";?>
+    <div class="about-page">
+        <h2>TEAM</h2>
+
+        <section id="team">
+
+            <div>
+                <h6>Amancio Ortega <span>founder</span></h6>
+                <img src="https://flying-dutchman.net/wp-content/uploads/2021/03/Amancio-Ortega-scaled.jpg" alt="">
+
+            </div>
+            <div>
+                <h6>Rosalia Mera <span>co-founder</span></h6>
+                <img src="https://imageio.forbes.com/specials-images/imageserve/70e206fb1fbe09eceb0617d193652f84/0x0.jpg?format=jpg&height=416&width=416&fit=bounds" alt="">
+
+            </div>
+
+        </section>
+        <section id="mission_vision">
+            <h2>MISSION AND VISION STATEMENT</h2>
+            <p>
+            <h5> Identifying Market Preferences</h5>
+            Slay Dragons declares in the first part of its mission statement that it intends to give customers what they want, and Slay Dragons definitely means it. Other high fashion brands do not really make an effort to identify the preferences of the target market. This has actually been the culture in high fashion for decades. The designers simply come up with apparel that matches their own tastes and styles and expect that people will buy it simply because it is high fashion.
+
+            The approach of Slay Dragons is exactly the opposite. While Slay Dragons does have a strong team of designers with impeccable talent, it also listens carefully to what the customers want. Slay Dragons has a very interactive social media presence, and it utilizes this to reach out to its client base, communicating with it to get a feel of what the market is going to buy. Even in physical stores, Slay Dragons staff take note of what customers like and do not like. The information is relayed to the design team, who makes sure that the client's requests and suggestions are incorporated into the brand's next collections.
+
+            <h5>Fast Fashion Strategy</h5>
+            In the second part of the Slay Dragons mission statement, the company aims for the speedy delivery of the market's demand. In other words, it does not waste any time in its production process. On average, it takes only two weeks for the Slay Dragons team to complete a collection that includes 500 or so designs. This includes everything from design conceptualization to delivering the clothing to the stores. This is what is now known in the retail industry as the fast fashion strategy.
+
+            The Slay Dragons vision statement is "to contribute to the sustainable development of society and that of the environment with which we interact."
+
+            A company's vision statement reflects its long-term goals and how it sees itself in the distant future. Like its mission statement, the Slay Dragons vision statement is clear and concise, and it also reveals a side of the fashion brand that we don't easily see while we are buying its clothes. The statement shows how the company puts a priority on society and the environment more than it does on sales and profits.
+
+            <h5>Societal Development</h5>
+            Slay Dragons takes its corporate social responsibility very seriously. It is constantly involved in campaigns for the improvement of people's lives and actively supports programs that give back to the community. In the light of recent criticisms received by the fashion industry regarding the use of inappropriate labor practices in its third-world country facilities, brands like Slay Dragons are taking the extra effort to declare their support for societal development and sustainability.
+
+            <h5>Environmental Sustainability</h5>
+            The environment is also very important to the Slay Dragons brand. It is quite known for its active stance in environmental campaigns. It has long denounced the use of plastic and does not include unnecessary labels on its clothing. Its manufacturing process is also completely toxic-free.</p>
+
+        </section>
+
+        <section id="information">
+            <h2>BUSINESS BRIEF</h2>
+
+            <p>Slay Dragons is one of the largest international fashion companies. It belongs to Inditex, one of the world’s largest distribution groups.
+
+                The customer is at the heart of our unique business model, which includes design, production, distribution and sales through our extensive retail network.
+
+                For more information, please visit the Inditex Group website: www.inditex.com</p>
+        </section>
+
+    </div>
+    <?php include_once __DIR__ . "/footer.php";?>
+</body>
+
+</html>
